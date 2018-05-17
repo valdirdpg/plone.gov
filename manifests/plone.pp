174de2a2fd0efbca5795c61e2898340038eb36f2
valdirdpg <valdirdpg@hotmail.com>
2018-05-17 09:42:53 -0300

    Altera diretório e usuário para root
    Altera o usuário e o diretório que era ubuntu para root, e as versões de zc.buildout e setuptools pois estavam conflitantes por serem desatualizados: zc.buildout==2.9.6 setuptools==36.6.1, essas são as versões atuais.

--- manifests/plone.pp
+++ manifests/plone.pp
@@ -3,27 +3,27 @@ class plone {
     # XXX: Plone 4.3.15 doesn't have a buildout-cache.tar.bz2
     $plone_version = "4.3.14"
 
-    file { ['/home/ubuntu/tmp',
-            '/home/ubuntu/.buildout',
-            '/home/ubuntu/buildout-cache',
-            '/home/ubuntu/buildout-cache/eggs',
-            '/home/ubuntu/buildout-cache/downloads',
-            '/home/ubuntu/buildout-cache/extends',
+    file { ['/home/root/tmp',
+            '/home/root/.buildout',
+            '/home/root/buildout-cache',
+            '/home/root/buildout-cache/eggs',
+            '/home/root/buildout-cache/downloads',
+            '/home/root/buildout-cache/extends',
             ]:
         ensure => directory,
-        owner => 'ubuntu',
-        group => 'ubuntu',
+        owner => 'root',
+        group => 'root',
         mode => '0755',
     }
 
-    file { '/home/ubuntu/.buildout/default.cfg':
+    file { '/home/root/.buildout/default.cfg':
         ensure => present,
         content => inline_template('[buildout]
-eggs-directory = /home/ubuntu/buildout-cache/eggs
-download-cache = /home/ubuntu/buildout-cache/downloads
-extends-cache = /home/ubuntu/buildout-cache/extends'),
-        owner => 'ubuntu',
-        group => 'ubuntu',
+eggs-directory = /home/root/buildout-cache/eggs
+download-cache = /home/root/buildout-cache/downloads
+extends-cache = /home/root/buildout-cache/extends'),
+        owner => 'root',
+        group => 'root',
         mode => '0664',
     }
 
@@ -41,19 +41,19 @@ extends-cache = /home/ubuntu/buildout-cache/extends'),
     # create virtualenv
     exec {'virtualenv py27':
         alias => 'virtualenv',
-        creates => '/home/ubuntu/py27',
-        user => 'ubuntu',
-        cwd => '/home/ubuntu',
+        creates => '/home/root/py27',
+        user => 'root',
+        cwd => '/home/root',
         before => Exec['install_buildout_setuptools'],
         timeout => 300,
     }
 
     # install zc.buildout, setuptools
-    exec {'/home/ubuntu/py27/bin/pip install -U zc.buildout==2.9.5 setuptools==36.3.0':
+    exec {'/home/root/py27/bin/pip install -U zc.buildout==2.9.6 setuptools==36.6.1':
         alias => 'install_buildout_setuptools',
-        creates => '/home/ubuntu/py27/bin/buildout',
-        user => 'ubuntu',
-        cwd => '/home/ubuntu',
+        creates => '/home/root/py27/bin/buildout',
+        user => 'root',
+        cwd => '/home/root',
         before => Exec['clone_repo'],
         timeout => 0,
     }
@@ -61,20 +61,20 @@ extends-cache = /home/ubuntu/buildout-cache/extends'),
     # download the buildout-cache from dist.plone.org
     exec {"wget --no-verbose http://dist.plone.org/release/${plone_version}/buildout-cache.tar.bz2":
         alias => 'download_buildout_cache',
-        creates => '/home/ubuntu/buildout-cache.tar.bz2',
-        cwd => '/home/ubuntu',
-        user => 'ubuntu',
-        group => 'ubuntu',
+        creates => '/home/root/buildout-cache.tar.bz2',
+        cwd => '/home/root',
+        user => 'root',
+        group => 'root',
         before => Exec['unpack_buildout_cache'],
         timeout => 600,
     }
 
-    # unpack the buildout-cache to /home/ubuntu/buildout-cache/
-    exec {'tar xjf /home/ubuntu/buildout-cache.tar.bz2':
+    # unpack the buildout-cache to /home/root/buildout-cache/
+    exec {'tar xjf /home/root/buildout-cache.tar.bz2':
         alias => 'unpack_buildout_cache',
-        creates => '/home/ubuntu/buildout-cache/eggs/Products.CMFPlone-${plone_version}-py2.7.egg/',
-        user => 'ubuntu',
-        cwd => '/home/ubuntu',
+        creates => '/home/root/buildout-cache/eggs/Products.CMFPlone-${plone_version}-py2.7.egg/',
+        user => 'root',
+        cwd => '/home/root',
         before => Exec['clone_repo'],
         timeout => 0,
     }
@@ -83,7 +83,7 @@ extends-cache = /home/ubuntu/buildout-cache/extends'),
     exec {'git clone https://github.com/plonegovbr/portal.buildout.git buildout':
         alias => 'clone_repo',
         creates => '/vagrant/buildout',
-        user => 'ubuntu',
+        user => 'root',
         cwd => '/vagrant',
         before => Exec['set_default_conf'],
         timeout => 0,
@@ -93,17 +93,17 @@ extends-cache = /home/ubuntu/buildout-cache/extends'),
     exec {'ln -s travis-development.cfg buildout.cfg':
         alias => 'set_default_conf',
         creates => '/vagrant/buildout/buildout.cfg',
-        user => 'ubuntu',
+        user => 'root',
         cwd => '/vagrant/buildout',
         before => Exec['buildout'],
         timeout => 0,
     }
 
     # run IDG buildout
-    exec {'/home/ubuntu/py27/bin/buildout':
+    exec {'/home/root/py27/bin/buildout':
         alias => 'buildout',
         creates => '/vagrant/buildout/bin/instance',
-        user => 'ubuntu',
+        user => 'root',
         cwd => '/vagrant/buildout',
         # before => Exec['final'],
         timeout => 0,
